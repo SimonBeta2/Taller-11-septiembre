9dca383273d7237360938fa33816fa2fc7859a46
SimonBeta2 <simon.betancurs@udea.edu.co>
2024-09-11 19:14:51 -0500

    Delete target directory

--- target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"operaciones","l":"BinarioDecimal"},{"p":"com.mycompany.calculadora","l":"Calculadora"},{"p":"excepciones","l":"Excepciones"},{"p":"menu","l":"Menu"},{"p":"operaciones","l":"Operaciones"}];updateSearchResults();
