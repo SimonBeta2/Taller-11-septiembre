6f5ff9efe1fc1dca836a96389aeffc07d6f88644
SimonBeta2 <simon.betancurs@udea.edu.co>
2024-09-11 21:44:37 -0500

    Delete Calculadora directory

--- Calculadora/target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mycompany.calculadora"},{"l":"excepciones"},{"l":"menu"},{"l":"operaciones"}];updateSearchResults();
